19b3206c5bd4869ea1f16751a5fd36e1a29fde8c
Ruichao Zhang <zhangruichaotyler@gmail.com>
2020-11-28 10:44:29 -0500

    Setup assets imported, configure physics, add spinner movement and tilting logic, add trail effect

---
+++ GameProject/Assets/Scripts/MovementController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] Joystick joyStick = null;
+    [SerializeField] float speed = 2f;
+    [SerializeField] float maxDeltaVelocity = 4f;
+    [SerializeField] float tiltAmount = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(joyStick.Vertical * speed * tiltAmount, 0, -joyStick.Horizontal * speed * tiltAmount);
+    }
+
+    // Get direction from joystick
+    private Vector3 GetInputDir()
+    {
+        Vector3 xMove = Vector3.right * joyStick.Horizontal;
+        Vector3 yMove = Vector3.forward * joyStick.Vertical;
+        return (xMove + yMove).normalized;
+    }
+
+    // update physics movement in FixedUpdate
+    private void FixedUpdate()
+    {
+        Vector3 inputVel = GetInputDir() * speed;//velocity per frame
+        if (inputVel != Vector3.zero)
+        {
+            Vector3 vel = rb.velocity;
+            Vector3 deltaVel = inputVel - vel;
+            deltaVel.y = 0;
+            deltaVel.x = Mathf.Clamp(deltaVel.x, -maxDeltaVelocity, maxDeltaVelocity);
+            deltaVel.z = Mathf.Clamp(deltaVel.z, -maxDeltaVelocity, maxDeltaVelocity);
+
+            rb.AddForce(deltaVel, ForceMode.Acceleration);
+        }
+
+    }
+
+
+    // private member variables
+    private Rigidbody rb = null;
+}
---
+++ GameProject/Assets/Scripts/Spinner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 2800.0f;
+    [SerializeField] bool doSpin = true;
+    [SerializeField] GameObject playerGraphics = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doSpin)
+        {
+            playerGraphics.transform.Rotate(new Vector3(0, spinSpeed * Time.deltaTime, 0));
+        }
+    }
+
+    private Rigidbody rb = null;
+}
