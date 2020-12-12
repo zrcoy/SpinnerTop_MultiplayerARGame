548fe98c23da0858921d8f76b01e7fb61d883370
zrcoy <zhangruichaotyler@gmail.com>
2020-12-12 16:29:12 -0500

    take lag compensation into account

--- GameProject/Assets/Scripts/MySyncScript.cs
+++ GameProject/Assets/Scripts/MySyncScript.cs
@@ -6,26 +6,28 @@ using Photon.Pun;
 
 public class MySyncScript : MonoBehaviour, IPunObservable
 {
-
+    public bool syncVelocity = true;
+    public bool synvAngularVelocity = true;
+    public bool isTeleportEnabled = true;
+    public float teleportIfDistanceGreaterThan = 1.0f;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
-        photonView = GetComponent<PhotonView>();
+        m_rb = GetComponent<Rigidbody>();
+        m_photonView = GetComponent<PhotonView>();
 
-        networkedPosition = new Vector3();
-        networkedRotation = new Quaternion();
+        m_networkedPosition = new Vector3();
+        m_networkedRotation = new Quaternion();
     }
 
     void FixedUpdate()
     {
-        if (!photonView.IsMine)
+        if (!m_photonView.IsMine)
         {
-            rb.position = Vector3.MoveTowards(rb.position, networkedPosition, Time.fixedDeltaTime);
+            m_rb.position = Vector3.MoveTowards(m_rb.position, m_networkedPosition, m_distance * (1.0f / PhotonNetwork.SerializationRate));
             // * 100 to make changes more obvious on remote client
-            rb.rotation = Quaternion.RotateTowards(rb.rotation, networkedRotation, Time.fixedDeltaTime * 100);
+            m_rb.rotation = Quaternion.RotateTowards(m_rb.rotation, m_networkedRotation, m_angle * (1.0f / PhotonNetwork.SerializationRate));
         }
-
     }
 
 
@@ -35,24 +37,63 @@ public class MySyncScript : MonoBehaviour, IPunObservable
         {
             //if we are controlling the local player
             //we now should send our transform, velocity, etc..data to other players
-            stream.SendNext(rb.position);
-            stream.SendNext(rb.rotation);
+            stream.SendNext(m_rb.position);
+            stream.SendNext(m_rb.rotation);
+            if (syncVelocity)
+            {
+                stream.SendNext(m_rb.velocity);
+            }
+            if (synvAngularVelocity)
+            {
+                stream.SendNext(m_rb.angularVelocity);
+            }
         }
         else
         {
             // if we are reading data
             // called on my player gameobject that exist on other players game
-            networkedPosition = (Vector3)stream.ReceiveNext();
-            networkedRotation = (Quaternion)stream.ReceiveNext();
+            m_networkedPosition = (Vector3)stream.ReceiveNext();
+            m_networkedRotation = (Quaternion)stream.ReceiveNext();
 
+            if (isTeleportEnabled)
+            {
+                if (Vector3.Distance(m_rb.position, m_networkedPosition) > teleportIfDistanceGreaterThan)
+                {
+                    m_rb.position = m_networkedPosition;
+                }
+            }
+
+
+            if (synvAngularVelocity || syncVelocity)
+            {
+                //get the positive lag time between data send time and current consistent network received time
+                float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+
+                if (syncVelocity)
+                {
+                    m_rb.velocity = (Vector3)stream.ReceiveNext();
+                    m_networkedPosition += m_rb.velocity * lag;
+                    m_distance = Vector3.Distance(m_rb.position, m_networkedPosition);
+                }
+
+                if (synvAngularVelocity)
+                {
+                    m_rb.angularVelocity = (Vector3)stream.ReceiveNext();
+
+                    m_networkedRotation = Quaternion.Euler(m_rb.angularVelocity * lag) * m_networkedRotation;
+
+                    m_angle = Quaternion.Angle(m_rb.rotation, m_networkedRotation);
+                }
+            }
         }
     }
 
 
     //private fields
-    Rigidbody rb;
-    PhotonView photonView;
-
-    Vector3 networkedPosition;
-    Quaternion networkedRotation;
+    private Rigidbody m_rb;
+    private PhotonView m_photonView;
+    private Vector3 m_networkedPosition;
+    private Quaternion m_networkedRotation;
+    private float m_distance;
+    private float m_angle;
 }
